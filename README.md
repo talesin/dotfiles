5bf6db5331acb8e99e9f43be0c36a0408849e0b3
Jeremy Clough <jclough@seek.com.au>
2018-07-14 11:00:47 +1000

    tweaking macos setup

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# MacOS
+- Install [iTerm2](https://www.iterm2.com)
+- Install [Homebrew](https://brew.sh)
+
 # References
 
 - https://github.com/anishathalye/dotbot
--- macos-setup.sh
+++ macos-setup.sh
@@ -1,9 +1,27 @@
 #!/usr/bin/env bash
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install python direnv byobu vim coreutils fish
+DIR=`cd $(dirname $0)/..; pwd`
+pushd $DIR
+
+# install iTerm2
+if ! [ -d "/Applications/iTerm.app" ]; then
+   curl -o /Applications/iTerm.app  https://iterm2.com/downloads/stable/latest 
+fi
+
+# install brew
+which brew > /dev/null
+if [ $? -eq 0 ]; then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    brew update
+fi
+brew install coreutils git python direnv byobu vim fish
+
+# enable direnv
 direnv allow
 
 export PATH="/usr/local/opt/python/libexec/bin;${PATH}"
+
+# install powerline fonts
 pip install --user powerline-status
 
 pushd /tmp
@@ -14,8 +32,20 @@ cd ..
 rm -fr fonts
 popd
 
+# setup fish
 curl -L https://get.oh-my.fish | fish
 sudo echo /usr/local/bin/fish >> /etc/shells
 chsh -s /usr/local/bin/fish
 
+# install .dotfiles
+if ! [ -d "$HOME/.dotfiles" ]; then
+    git clone https://github.com/talesin/dotfiles.git $HOME/.dotfiles
+fi
+pushd $HOME/.dotfiles
+./install
+popd
+
+# setup byobu
 byobu-enable
+
+popd
