1e310ba93a4c0768bb113ef506bf215b26dbafab
Jeremy Clough <talesin@users.noreply.github.com>
2018-07-14 14:45:21 +1000

    Doc and clean (#3)
    * tweaking macos setup
    
    * tweaking macos setup
    
    * tweaking macos setup
    
    * tweaking macos setup
    
    * tweaking macos setup
    
    * tweaking macos setup
    
    * tweaking macos setup
    
    * tweaking macos setup

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
+# MacOS
+
+## Quick Install
+
+`curl -s https://raw.githubusercontent.com/talesin/dotfiles/doc-and-clean/macos-setup.sh | bash`
+
+## Manual Install
+
+- Install [iTerm2](https://www.iterm2.com)
+- Install [Homebrew](https://brew.sh)
+
 # References
 
 - https://github.com/anishathalye/dotbot
--- macos-setup.sh
+++ macos-setup.sh
@@ -1,9 +1,30 @@
 #!/usr/bin/env bash
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install python direnv byobu vim coreutils fish
+DIR=`cd $(dirname $0); pwd`
+pushd $DIR
+
+# download iTerm2
+if ! [ -d "/Applications/iTerm.app" ]; then
+    pushd $HOME/Downloads
+    curl -L -s -o iTerm.zip https://iterm2.com/downloads/stable/latest
+    unzip iTerm.zip
+    mv iTerm.app /Applications
+    popd
+fi
+
+# install brew
+which brew > /dev/null
+if [ $? -eq 1 ]; then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    brew update
+fi
+brew install coreutils git python direnv byobu vim fish
+
+# enable direnv
 direnv allow
 
-export PATH="/usr/local/opt/python/libexec/bin;${PATH}"
+# install powerline fonts
+export PATH="/usr/local/opt/python/libexec/bin:${PATH}"
 pip install --user powerline-status
 
 pushd /tmp
@@ -14,8 +35,26 @@ cd ..
 rm -fr fonts
 popd
 
-curl -L https://get.oh-my.fish | fish
-sudo echo /usr/local/bin/fish >> /etc/shells
+# setup fish
+pushd $HOME/Downloads
+curl -s -L https://get.oh-my.fish > omf-install.fish
+fish omf-install.fish
+popd
+grep -sq fish /etc/shells
+if [ $? -eq 1 ]; then
+    sudo sh -c "echo /usr/local/bin/fish >> /etc/shells"
+fi
 chsh -s /usr/local/bin/fish
 
-byobu-enable
+# install .dotfiles
+if ! [ -d "$HOME/.dotfiles" ]; then
+    git clone --recurse-submodules https://github.com/talesin/dotfiles.git $HOME/.dotfiles
+fi
+pushd $HOME/.dotfiles
+./install
+popd
+
+# setup byobu
+/usr/local/bin/byobu-enable
+
+popd
