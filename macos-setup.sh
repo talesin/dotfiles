96881c8d3e74539112edac88c57d71abed158df9
Jeremy Clough <jclough@seek.com.au>
2018-07-30 21:59:37 +1000

    addimg omf theme

--- macos-setup.sh
+++ macos-setup.sh
@@ -34,6 +34,8 @@ rm -fr fonts
 curl -s -L https://get.oh-my.fish > omf-install.fish
 fish omf-install.fish --noninteractive
 rm omf-install.fish
+fish -c omf theme install agnoster
+fish -c omf theme agnoster
 grep -sq fish /etc/shells
 if [ $? -eq 1 ]; then
     sudo sh -c "echo /usr/local/bin/fish >> /etc/shells"
