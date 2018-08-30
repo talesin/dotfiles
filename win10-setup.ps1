222270a9d671969c38bdc6f1fa20763bbcb4be9c
Jeremy Clough <jclough@seek.com.au>
2018-08-30 17:00:46 +1000

    broken setup

--- win10-setup.ps1
+++ win10-setup.ps1
@@ -5,13 +5,17 @@ iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/in
 
 choco install conemu
 choco install git.install
+choco install powershell-core --pre
 
 # install powerline fonts
 pushd $env:TEMP
 git clone https://github.com/powerline/fonts.git
 cd fonts
-.\install.ps1
+pswh -File .\install.ps1
 cd ..
 del -recurse -force fonts
 popd
 
+pwsh -c { Install-Module posh-git -Scope CurrentUser }
+pwsh -c { Install-Module oh-my-posh -Scope CurrentUser }
+pwsh -c { Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force }
