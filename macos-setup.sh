99b6be988bed70803dec9608d0feecbe1a00047e
Jeremy Clough <jeremy@talesin.net>
2019-03-16 18:31:44 +1100

    added Brewfile to downloads

--- macos-setup.sh
+++ macos-setup.sh
@@ -9,6 +9,10 @@ function install-brew() {
 }
 
 function install-apps {
+    if [ -f Brewfile ]; then
+        curl -s 'https://raw.githubusercontent.com/talesin/dotfiles/bash-tweaks/Brewfile' -o Brewfile
+    fi
+
     brew update
     brew bundle
     brew cleanup
